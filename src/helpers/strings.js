b507263da2eaff6b8647b7fe30d90e7f8ad9ccbe
LuisJOrtiz <ljoo259@gmail.com>
2019-10-01 02:34:05 -0500

    Created Strings helper.

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -1,1 +1,25 @@
-//You can rename this file if you want
+'use strict'
+
+err_Status = 'Estado de usuario inválido'
+err_Type = 'Tipo de usuario inválido'
+err_day = 'Día inválido'
+err_Name_empty = 'Por favor ingrese el nombre'
+err_Code_empty = 'Por favor ingrese el código'
+err_Username_empty = 'Por favor ingrese el usuario'
+err_Password = 'Contraseña inválida'
+err_Password_empty = 'Por favor ingrese su contraseña'
+err_Password_length = 'La contraseña debe contener 8 dígitos como mínimo'
+err_Cellphone = 'Número de teléfono invalido'
+err_Cellphone_empty = 'Por favor ingrese el número de teléfono'
+
+
+module.exports = {
+    err_Status,
+    err_Type,
+    err_day,
+    err_Name_empty,
+    err_Code_empty,
+    err_Username_empty,
+    err_Password,err_Password_empty,err_Password_length,
+    err_Cellphone,err_Cellphone_empty
+}
