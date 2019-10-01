b8d9fa4cf4ef2f853bb86091a8a35fe242dec6a3
Carlos Figueroa <cafb97@gmail.com>
2019-09-30 20:33:06 -0500

    [FIX] - Strings unified.

--- src/helpers/strings.js
+++ src/helpers/strings.js
@@ -2,8 +2,12 @@
 
 const Enum = require('enum')
 
-var StatusType = new Enum({ 1: 'pending', 2: 'active', 3: 'inactive', 4: 'banned' })
-var StatusTypeClient = new Enum({ 1: 'unknown', 2: 'active', 3: 'inactive', 4: 'banned' })
+const active = 'active'
+const inactive = 'inactive'
+const banned = 'banned'
+
+var StatusType = new Enum({ 1: 'pending', 2: active, 3: inactive, 4: banned })
+var StatusTypeClient = new Enum({ 1: 'unknown', 2: active, 3: inactive, 4: banned })
 var UserType = new Enum({ 1: 'client', 2: 'deliveryman', 3: 'restaurantadmin', 4: 'restaurantemployee', 5: 'thirdparty' })
 var DeliverymanType = new Enum({ 1: 'fixed', 2:'nonfixed' })
 
